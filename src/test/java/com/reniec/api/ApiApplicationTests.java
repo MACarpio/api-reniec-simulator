3c10e42be72aadc4161def6f5203927239bc1c37
Mauricio Apaza Carpio <mpacarpio@gmail.com>
2022-09-15 00:23:53 -0500

    first commit

---
+++ src/test/java/com/reniec/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reniec.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
