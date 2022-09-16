bdb14cc85bc81782f40f906011cd87cc9acb66ba
Edwinmrc <edwin_ramos2@usmp.pe>
2022-09-15 19:08:50 -0500

    Registro y Login

---
+++ src/main/java/com/reniec/api/model/Usuario.java
@@ -0,0 +1,24 @@
+package com.reniec.api.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Id;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "t_user")
+public class Usuario implements Serializable {
+    @Id
+    @Column(name = "user_id")
+    private String userID;
+    private String password;
+    private String tipoUsuario;
+    
+}
