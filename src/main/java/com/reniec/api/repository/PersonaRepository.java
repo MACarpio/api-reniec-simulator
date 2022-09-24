12f14ccf49f71489bde68cf843cbb729b6ee123c
Mauricio Apaza Carpio <mpacarpio@gmail.com>
2022-09-24 13:05:55 -0500

    resolve forms

--- src/main/java/com/reniec/api/repository/PersonaRepository.java
+++ src/main/java/com/reniec/api/repository/PersonaRepository.java
@@ -1,15 +1,9 @@
 package com.reniec.api.repository;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import com.reniec.api.model.Persona;
-
-import java.util.List;
 
 @Repository
 public interface PersonaRepository extends JpaRepository<Persona, String> {
